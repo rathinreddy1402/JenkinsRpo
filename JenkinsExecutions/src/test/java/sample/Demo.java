1183f8ff4c0ae2e8905bed61e9c6656c62b07569
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-07-12 22:30:03 +0530

    1

--- JenkinsExecutions/src/test/java/sample/Demo.java
+++ JenkinsExecutions/src/test/java/sample/Demo.java
@@ -1,14 +1,32 @@
 package sample;
 
-public class Demo {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
+public class Demo 
+{
 	
-		System.out.println("method running");
-
-		//jenkins
-		//push
-		//execute
+@Test
+		public void m1()
+		{
+	WebDriver driver;
+	
+	 String BROWSER = System.getProperty("browser");
+	String URL = System.getProperty("url");
+		
+	if(BROWSER.equalsIgnoreCase("chrome"))
+	{
+		driver=new ChromeDriver();
 	}
+	else if(BROWSER.equalsIgnoreCase("edge"))
+	{
+		driver=new EdgeDriver();
+	}
+	driver=new ChromeDriver();
+		}
 
-}
+		}
+
+
